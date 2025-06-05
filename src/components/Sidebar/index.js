8c3c2ff327a574c3ac234c1912655eda4c27a5f5
Ivan <ivanlazarov2003@outlook.com>
2025-06-05 17:41:30 +0200

    Made home page and layout

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,78 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtittle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faLinkedin,
+  faGithub,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtittle} alt="logo" />
+      </Link>
+
+      <nav>
+        <NavLink exact="true" activeclassname="active" to={'/'}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to={'/about'}
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to={'/contact'}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+
+      <ul>
+        <li>
+          <a
+            target="blank"
+            rel="noreferer"
+            href="https://x.com/ivanlazarov2003"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="blank"
+            rel="noreferer"
+            href="https://github.com/IvanLazarov03"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="blank"
+            rel="noreferer"
+            href="https://www.instagram.com/lazarov37/"
+          >
+            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
